yegappan/lsp

commit 6e4ba228eaa0770470060d8d47a360bd05a04ee5

Diags are not highlighted after a buffer is reloaded

autoload/lsp/diag.vim
@@ -208,7 +208,7 @@ enddef
 
 # Refresh the placed diagnostics in buffer "bnr"
 # This inline signs, inline props, and virtual text diagnostics
-def DiagsRefresh(bnr: number)
+export def DiagsRefresh(bnr: number)
   :silent! bnr->bufload()
 
   RemoveDiagVisualsForBuffer(bnr)

autoload/lsp/lsp.vim
@@ -529,6 +529,23 @@ export def RemoveFile(bnr: number): void
   endfor
 enddef
 
+# Buffer 'bnr' is loaded in a window, send the latest buffer contents to the
+# language servers.
+export def BufferLoadedInWin(bnr: number)
+  var lspservers: list<dict<any>> = buf.BufLspServersGet(bnr)
+  if lspservers->empty()
+    # No language servers for this buffer
+    return
+  endif
+  for lspserver in lspservers
+    if !lspserver->empty() && lspserver.ready
+      lspserver.textdocDidChange(bnr, 0, 0, 0, [])
+    endif
+  endfor
+  # Refresh the displayed diags visuals
+  diag.DiagsRefresh(bnr)
+enddef
+
 # Stop all the LSP servers
 export def StopAllServers()
   for lspserver in LSPServers

autoload/lsp/lspserver.vim
@@ -543,7 +543,8 @@ def TextdocDidOpen(lspserver: dict<any>, bnr: number, ftype: string): void
     textDocument: {
       uri: util.LspBufnrToUri(bnr),
       languageId: ftype,
-      version: 1,
+      # Use Vim 'changedtick' as the LSP document version number
+      version: bnr->getbufvar('changedtick'),
       text: bnr->getbufline(1, '$')->join("\n") .. "\n"
     }
   }
@@ -570,7 +571,7 @@ def TextdocDidChange(lspserver: dict<any>, bnr: number, start: number,
   # Notification: 'textDocument/didChange'
   # Params: DidChangeTextDocumentParams
 
-  var changeset: list<dict<any>>
+  # var changeset: list<dict<any>>
 
   ##### FIXME: Sending specific buffer changes to the LSP server doesn't
   ##### work properly as the computed line range numbers is not correct.
@@ -608,14 +609,15 @@ def TextdocDidChange(lspserver: dict<any>, bnr: number, start: number,
   #   changeset->add({'range': range, 'text': lines})
   # endfor
 
-  changeset->add({text: bnr->getbufline(1, '$')->join("\n") .. "\n"})
   var params = {
     textDocument: {
       uri: util.LspBufnrToUri(bnr),
       # Use Vim 'changedtick' as the LSP document version number
       version: bnr->getbufvar('changedtick')
     },
-    contentChanges: changeset
+    contentChanges: [
+      {text: bnr->getbufline(1, '$')->join("\n") .. "\n"}
+    ]
   }
   lspserver.sendNotification('textDocument/didChange', params)
 enddef

plugin/lsp.vim
@@ -49,6 +49,7 @@ augroup LSPAutoCmds
   # Note that when BufWipeOut is invoked, the current buffer may be different
   # from the buffer getting wiped out.
   autocmd BufWipeOut * lsp.RemoveFile(expand('<abuf>')->str2nr())
+  autocmd BufWinEnter * lsp.BufferLoadedInWin(expand('<abuf>')->str2nr())
 augroup END
 
 # TODO: Is it needed to shutdown all the LSP servers when exiting Vim?

test/clangd_tests.vim
@@ -1548,6 +1548,31 @@ def g:Test_InlayHints()
   :%bw!
 enddef
 
+# Test for reloading a modified buffer with diags
+def g:Test_ReloadBufferWithDiags()
+  var lines: list<string> =<< trim END
+    void ReloadBufferFunc1(void)
+    {
+      int a:
+    }
+  END
+  writefile(lines, 'Xreloadbuffer.c')
+  :silent! edit Xreloadbuffer.c
+  g:WaitForServerFileLoad(1)
+  var signs = sign_getplaced('%', {group: '*'})[0].signs
+  assert_equal(3, signs[0].lnum)
+  append(0, ['', ''])
+  signs = sign_getplaced('%', {group: '*'})[0].signs
+  assert_equal(5, signs[0].lnum)
+  :edit!
+  sleep 200m
+  signs = sign_getplaced('%', {group: '*'})[0].signs
+  assert_equal(3, signs[0].lnum)
+
+  :%bw!
+  delete('Xreloadbuffer.c')
+enddef
+
 # Test for the :LspServer command.
 def g:Test_LspServer()
   new a.raku

commit 6f93e751059f59009b5608539272380dcdbb16db

Never show diagnostics when they are disabled
A regression was introduced in 6e4ba228eaa0770470060d8d47a360bd05a04ee5
that causes diagnostics to be displayed on buffer reload, even when the
user disabled them.

Fix this by moving the option check directly to DiagsRefresh.

autoload/lsp/diag.vim
@@ -215,6 +215,11 @@ enddef
 # Refresh the placed diagnostics in buffer "bnr"
 # This inline signs, inline props, and virtual text diagnostics
 export def DiagsRefresh(bnr: number, all: bool = false)
+  var lspOpts = opt.lspOptions
+  if !lspOpts.autoHighlightDiags || lspOpts.aleSupport
+    return
+  endif
+
   :silent! bnr->bufload()
 
   RemoveDiagVisualsForBuffer(bnr, all)
@@ -228,7 +233,6 @@ export def DiagsRefresh(bnr: number, all: bool = false)
   var diag_align: string = 'above'
   var diag_wrap: string = 'truncate'
   var diag_symbol: string = '┌─'
-  var lspOpts = opt.lspOptions
 
   if lspOpts.diagVirtualTextAlign == 'below'
     diag_align = 'below'
@@ -331,9 +335,6 @@ export def ProcessNewDiags(bnr: number)
   var lspOpts = opt.lspOptions
   if lspOpts.aleSupport
     SendAleDiags(bnr, -1)
-    return
-  elseif !lspOpts.autoHighlightDiags
-    return
   endif
 
   if bnr == -1 || !diagsMap->has_key(bnr)